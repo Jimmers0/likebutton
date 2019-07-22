02cef7c4d26e08ccf4fdb0f4b3668e619b4eb638
Jamie Randall <bentleyx007@gmail.com>
2019-07-22 16:11:42 -0700

    likebutton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+
+
+  add = (e) => {
+
+    e.preventDefault();
+      this.setState({
+      counter: this.state.counter + 1,
+})
+  console.log(this.state.counter + 1)
+
+  if (this.state.counter + 1 === 1) {
+    this.setState({
+      like: this.state.like = "like"
+    }) 
+  } else {
+    this.setState({
+      like: this.state.like = "likes"
+    })
+  }
+  }
+
+  state = {
+    counter: 0,
+    like: "likes"
+  }
+
+  render(){
+    return (
+      <div>
+       <button onClick={this.add}>{this.state.counter} {this.state.like}</button>
+       
+
+      </div>
+    )
+  }
 }
 
 export default App;
